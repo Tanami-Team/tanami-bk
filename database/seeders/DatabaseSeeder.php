323dae582fea86cce6702b42dc2fbaf8d7242ee5
ahmed fouad <asd09505@gmail.com>
2025-05-01 12:28:10 +0200

    a

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,5 +24,7 @@ class DatabaseSeeder extends Seeder
         $this->call(SliderSeeder::class);
         $this->call(SettingSeeder::class);
         $this->call(ProjectSeeder::class);
+        $this->call(MembersSeeder::class);
+        $this->call(GovernanceSeeder::class);
     }
 }
